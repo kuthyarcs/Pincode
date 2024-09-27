41d11c1f46db688f6590d0a4c56e12efa928fe2e
ckuthyar <ckuthyar@gmail.com>
2024-09-27 07:17:11 +0530

    Pincode2 REST API

---
+++ src/main/java/pkg1/Pincode2Application.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pincode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pincode2Application.class, args);
+	}
+
+}
